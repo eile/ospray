76dff50d8a1bad15cecdf261dfaee48c532c07fc
Stefan Eilemann <SEilemann@esri.com>
2020-05-26 11:19:23 +0200

    Mock OSPRay setup

--- apps/ospTutorial/ospTutorial.cpp
+++ apps/ospTutorial/ospTutorial.cpp
@@ -30,6 +30,22 @@
 #include <alloca.h>
 #endif
 
+// #define NORMALS
+// #define PATHTRACER
+// #define VISIBILITY
+
+#ifdef VISIBILITY
+#ifndef PATHTRACER
+#define PATHTRACER
+#endif
+#endif
+
+#ifdef PATHTRACER
+const static std::string rendererType = "pathtracer";
+#else
+const static std::string rendererType = "scivis";
+#endif
+
 using namespace ospcommon::math;
 
 typedef std::shared_ptr<boost::asio::io_service> io_service_ptr;
@@ -88,6 +104,7 @@ class Server
                     boost::asio::ip::address().from_string(interface), port)),
         _acceptor(*_ioServices.front(), _endpoint)
   {
+    _setupOSP();
     _startAccept();
   }
 
@@ -115,9 +132,119 @@ class Server
     }
   }
 
+  void _setupOSP()
+  {
+    // create and setup camera
+    _camera.setParam("aspect", _size.x / (float)_size.y);
+    _camera.setParam("position", vec3f{0.f, 0.f, 0.f});
+    _camera.setParam("direction", vec3f{0.1f, 0.f, 1.f});
+    _camera.setParam("up", vec3f{0.f, 1.f, 0.f});
+
+#ifndef VISIBILITY
+    // create and setup light for Ambient Occlusion
+    ospray::cpp::Light lights[] = {{"ambient"}, _headlight, {"distant"}};
+    _camera.setParam("apertureRadius", .15f);
+
+    lights[0].setParam("intensity", .8f);
+    lights[0].setParam("color", vec3f{1.f, 1.f, 1.f});
+    lights[0].commit();
+
+    lights[1].setParam("intensity", 1.f);
+    lights[1].setParam("color", vec3f{1.f, 1.f, 1.f});
+    lights[1].setParam("angularDiameter", 5);
+    lights[1].setParam("direction", vec3f{.7f, -.7f, 0.014f});
+    lights[1].setParam("position", vec3f{0, 0, 0});
+    lights[1].commit();
+
+    lights[2].setParam("intensity", 1.f);
+    lights[2].setParam("angularDiameter", 0.53);
+#ifdef PATHTRACER
+    lights[2].setParam("color", vec3f{3.f, 3.f, 3.f});
+#else
+    lights[2].setParam("color", vec3f{2.f, 2.f, 2.f});
+#endif
+    lights[2].setParam("direction", vec3f{-1.f, 0.f, 0.f});
+    lights[2].commit();
+
+    _lights = {lights[0].handle(), lights[1].handle(), lights[2].handle()};
+    ospray::cpp::Data lightData(3, OSP_LIGHT, _lights.data());
+
+    lightData.commit();
+    _renderer.setParam("lights", lightData);
+#endif
+
+// create and setup material
+#ifdef PATHTRACER
+    _material.setParam("Kd", vec3f{0.f, 0.f, 0.f});
+    _material.setParam("Ks", vec3f{1.f, 1.f, 1.f});
+    _material.setParam("Ns", 100.f);
+    _renderer.setParam("maxDepth", 1);
+#else
+    _material.setParam("Kd", vec3f{1.f, 1.f, 1.f});
+#endif
+    _material.commit();
+    _camera.commit(); // commit each object to indicate modifications are done
+
+    _emissive.setParam("color", vec3f{1000.f, 10.f, 0.f});
+    _emissive.setParam("intensity", 1.f);
+    _emissive.commit();
+
+    { // remove block
+      std::vector<vec3f> vertex = {vec3f(-1.0f, -1.0f, 3.0f),
+          vec3f(-1.0f, 1.0f, 3.0f),
+          vec3f(1.0f, -1.0f, 3.0f),
+          vec3f(0.1f, 0.1f, 0.3f)};
+
+      std::vector<vec4f> color = {vec4f(0.9f, 0.5f, 0.5f, 1.0f),
+          vec4f(0.8f, 0.8f, 0.8f, 1.0f),
+          vec4f(0.8f, 0.8f, 0.8f, 1.0f),
+          vec4f(0.5f, 0.9f, 0.5f, 1.0f)};
+
+      std::vector<vec3ui> index = {vec3ui(0, 1, 2), vec3ui(1, 2, 3)};
+
+      ospray::cpp::Geometry mesh("mesh");
+      mesh.setParam("vertex.position", ospray::cpp::Data(vertex));
+      mesh.setParam("vertex.color", ospray::cpp::Data(color));
+      mesh.setParam("index", ospray::cpp::Data(index));
+      mesh.commit();
+
+      // put the mesh into a model
+      ospray::cpp::GeometricModel model(mesh);
+      model.commit();
+
+      // put the model into a group (collection of models)
+      _group.setParam("geometry", ospray::cpp::Data(model));
+    }
+
+    _group.commit();
+    _instance.commit();
+    _world.setParam("instance", ospray::cpp::Data(_instance));
+    _world.commit();
+
+    // complete setup of renderer
+    _renderer.setParam("aoSamples", 1);
+    _renderer.setParam("aoTransparencyEnabled", true);
+    _renderer.setParam("shadowsEnabled", true);
+    _renderer.setParam("backgroundColor", 1.0f); // white, transparent
+    _renderer.setParam("epsilon", 3 * std::numeric_limits<float>::epsilon());
+    _renderer.commit();
+  }
+
   io_services _ioServices;
   const boost::asio::ip::tcp::endpoint _endpoint;
   boost::asio::ip::tcp::acceptor _acceptor;
+
+  vec2i _size{1024, 576};
+  ospray::cpp::Camera _camera{"perspective"};
+  ospray::cpp::Light _headlight{"sphere"};
+  std::vector<OSPLight> _lights{3};
+  ospray::cpp::Material _material{rendererType, "OBJMaterial"};
+  ospray::cpp::Material _emissive{rendererType, "Luminous"};
+  ospray::cpp::Renderer _renderer{rendererType};
+
+  ospray::cpp::Group _group;
+  ospray::cpp::Instance _instance{_group};
+  ospray::cpp::World _world;
 };
 } // namespace
 
